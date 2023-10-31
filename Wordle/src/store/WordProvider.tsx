fc31c480e3f5fb180cad8d67a328fe891c900d32
Chris <chriskang0917@gmail.com>
2023-10-31 22:33:32 +0800

    Feat: Key in letter by keyboard

--- Wordle/src/store/WordProvider.tsx
+++ Wordle/src/store/WordProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 
 export interface IWordContext {
   wordRecords: string[];
@@ -6,7 +6,10 @@ export interface IWordContext {
   currentRowIndex: number;
 }
 
-interface Action {}
+interface Action {
+  type: "add_letter" | "remove_letter";
+  payload?: string;
+}
 
 export interface InitContext {
   state: IWordContext;
@@ -18,7 +21,7 @@ interface WordProviderProps {
 }
 
 const initialState: IWordContext = {
-  wordRecords: ["HELLO", "HOW", "", "", "", ""],
+  wordRecords: ["HELLO", "", "", "", "", ""],
   answerWord: "ALLOW",
   currentRowIndex: 1,
 };
@@ -26,13 +29,49 @@ const initialState: IWordContext = {
 const WordContext = createContext<InitContext | IWordContext>(initialState);
 
 const wordReducer = (state: IWordContext, action: Action) => {
-  console.log("state", state);
-  console.log("action", action);
+  const currentWord = state.wordRecords[state.currentRowIndex];
+
+  switch (action.type) {
+    case "add_letter":
+      if (currentWord.length < 5) {
+        const updatedWordRecords = [...state.wordRecords];
+        updatedWordRecords[state.currentRowIndex] =
+          currentWord + action.payload?.toUpperCase();
+        return { ...state, wordRecords: updatedWordRecords };
+      }
+
+      return state;
+
+    case "remove_letter":
+      if (currentWord.length > 0) {
+        const updatedWordRecords = [...state.wordRecords];
+        updatedWordRecords[state.currentRowIndex] = currentWord.slice(0, -1);
+        return { ...state, wordRecords: updatedWordRecords };
+      }
+
+      return state;
+  }
+
   return initialState;
 };
 
 export const WordProvider: React.FC<WordProviderProps> = ({ children }) => {
   const [state, dispatch] = useReducer(wordReducer, initialState);
+
+  useEffect(() => {
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (/^[a-zA-Z]$/.test(e.key)) {
+        dispatch({ type: "add_letter", payload: e.key });
+      } else if (e.key === "Backspace") {
+        dispatch({ type: "remove_letter", payload: "" });
+      }
+    };
+
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
   return (
     <WordContext.Provider value={{ state, dispatch }}>
