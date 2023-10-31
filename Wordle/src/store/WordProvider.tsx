870d31b23b0d472ce85fd60d6589c40797b76599
Chris <chriskang0917@gmail.com>
2023-10-31 22:55:47 +0800

    Feat: Basic wordle logic

--- Wordle/src/store/WordProvider.tsx
+++ Wordle/src/store/WordProvider.tsx
@@ -4,10 +4,11 @@ export interface IWordContext {
   wordRecords: string[];
   answerWord: string;
   currentRowIndex: number;
+  hasWin: boolean;
 }
 
 interface Action {
-  type: "add_letter" | "remove_letter";
+  type: "add_letter" | "remove_letter" | "check_answer";
   payload?: string;
 }
 
@@ -21,38 +22,58 @@ interface WordProviderProps {
 }
 
 const initialState: IWordContext = {
-  wordRecords: ["HELLO", "", "", "", "", ""],
+  wordRecords: ["", "", "", "", "", ""],
   answerWord: "ALLOW",
-  currentRowIndex: 1,
+  currentRowIndex: 0,
+  hasWin: false,
 };
 
 const WordContext = createContext<InitContext | IWordContext>(initialState);
 
 const wordReducer = (state: IWordContext, action: Action) => {
-  const currentWord = state.wordRecords[state.currentRowIndex];
+  const currentWord = state.wordRecords[state.currentRowIndex] || "";
+  const maxRow: number = 6;
 
-  switch (action.type) {
-    case "add_letter":
-      if (currentWord.length < 5) {
-        const updatedWordRecords = [...state.wordRecords];
-        updatedWordRecords[state.currentRowIndex] =
-          currentWord + action.payload?.toUpperCase();
-        return { ...state, wordRecords: updatedWordRecords };
-      }
+  if (!state.hasWin && state.currentRowIndex < 6) {
+    switch (action.type) {
+      case "add_letter":
+        if (currentWord.length < 5) {
+          const updatedWordRecords = [...state.wordRecords];
+          updatedWordRecords[state.currentRowIndex] =
+            currentWord + action.payload?.toUpperCase();
+          return { ...state, wordRecords: updatedWordRecords };
+        }
 
-      return state;
+        return state;
 
-    case "remove_letter":
-      if (currentWord.length > 0) {
-        const updatedWordRecords = [...state.wordRecords];
-        updatedWordRecords[state.currentRowIndex] = currentWord.slice(0, -1);
-        return { ...state, wordRecords: updatedWordRecords };
-      }
+      case "remove_letter":
+        if (currentWord.length > 0) {
+          const updatedWordRecords = [...state.wordRecords];
+          updatedWordRecords[state.currentRowIndex] = currentWord.slice(0, -1);
+          return { ...state, wordRecords: updatedWordRecords };
+        }
 
-      return state;
+        return state;
+
+      case "check_answer":
+        if (state.currentRowIndex < maxRow && currentWord.length === 5) {
+          if (state.answerWord === currentWord) {
+            console.log("You win!");
+            return {
+              ...state,
+              hasWin: true,
+              currentRowIndex: state.currentRowIndex + 1,
+            };
+          }
+
+          return { ...state, currentRowIndex: state.currentRowIndex + 1 };
+        }
+
+        return state;
+    }
   }
 
-  return initialState;
+  return { ...initialState, hasWin: false };
 };
 
 export const WordProvider: React.FC<WordProviderProps> = ({ children }) => {
@@ -62,8 +83,14 @@ export const WordProvider: React.FC<WordProviderProps> = ({ children }) => {
     const handleKeyUp = (e: KeyboardEvent) => {
       if (/^[a-zA-Z]$/.test(e.key)) {
         dispatch({ type: "add_letter", payload: e.key });
-      } else if (e.key === "Backspace") {
-        dispatch({ type: "remove_letter", payload: "" });
+      }
+
+      if (e.key === "Backspace") {
+        dispatch({ type: "remove_letter" });
+      }
+
+      if (e.key === "Enter") {
+        dispatch({ type: "check_answer" });
       }
     };
 
