47d3763f31ef3f78f08b764f4d1d9b2c9855148b
Chris <chriskang0917@gmail.com>
2023-10-31 14:08:29 +0800

    Fix: Update display word logic

--- Wordle/src/components/Word.tsx
+++ Wordle/src/components/Word.tsx
@@ -6,21 +6,13 @@ interface WordProps {
 
 export default function Word({ word }: WordProps) {
   const splitWord: string[] = word.split("");
+  const remainingLetter: string[] = Array(5 - splitWord.length).fill("");
 
-  if (!word) {
-    const emptyLetters: string[] = Array(5).fill("");
-    return (
-      <div className="gap grid grid-cols-5 gap-x-1">
-        {emptyLetters.map((letter, index) => (
-          <Letter key={index} letter={letter} />
-        ))}
-      </div>
-    );
-  }
+  const fullWord: string[] = [...splitWord, ...remainingLetter];
 
   return (
     <div className="gap grid grid-cols-5 gap-x-1">
-      {splitWord.map((letter, index) => (
+      {fullWord.map((letter, index) => (
         <Letter key={index} letter={letter} />
       ))}
     </div>
